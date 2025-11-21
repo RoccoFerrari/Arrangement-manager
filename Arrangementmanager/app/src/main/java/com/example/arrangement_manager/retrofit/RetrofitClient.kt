575af885f2a09c7cf9fdbe75205a04986533116c
RoccoFerrari <emsicoding@gmail.com>
2025-11-21 20:48:45 +0100

    feat: Updated the AndroidManifest.xml file to include new permissions and modified the notification service. Updated RetrofitClient.kt with a new base URL and connection timeout. Modified the network security configuration file to disable clear traffic.

--- Arrangementmanager/app/src/main/java/com/example/arrangement_manager/retrofit/RetrofitClient.kt
+++ Arrangementmanager/app/src/main/java/com/example/arrangement_manager/retrofit/RetrofitClient.kt
@@ -6,6 +6,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 /**
  * Singleton object for configuring and providing a Retrofit client.
@@ -18,9 +19,13 @@ object RetrofitClient {
     // Flask server IP
     // Don't use 127.0.0.1 because this address refers to the emulator itself.
     // Use http://10.0.2.2:5000/ to reference the localhost.
-    private const val BASE_URL = "http://10.0.2.2:5000/" // LH
-    // private const val BASE_URL = "http://192.168.1.17:5000/" // LT
-    // private const val BASE_URL = "http://192.168.1.108:5000/" // Sottosopra
+    // private const val BASE_URL = "http://10.0.2.2:5000/" // LH
+     //private const val BASE_URL = "http://192.168.1.17:5000/" // LT
+     // private const val BASE_URL = "http://172.20.10.3:5000/" // Hotspot
+    // private const val BASE_URL = "http://192.168.1.109:5000/" // Sottosopra
+    private const val BASE_URL = "https://arrangement-manager.roccoferrari.com/"
+
+    // private const val BASE_URL = "http://192.168.1.106:5000/" // Sottosopra
 
     /**
      * Moshi instance for JSON parsing.
@@ -43,6 +48,9 @@ object RetrofitClient {
      */
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
         .build()
 
     /**
